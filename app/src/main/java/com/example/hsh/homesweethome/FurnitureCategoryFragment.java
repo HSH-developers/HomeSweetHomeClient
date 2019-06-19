db472e738799e057c0d7e009bc7a9268621c81fa
Teo Jun Jie <teojunjie@gmail.com>
2019-06-20 01:04:28 +0800

    Added filter dialog fragment

--- app/src/main/java/com/example/hsh/homesweethome/FurnitureCategoryFragment.java
+++ app/src/main/java/com/example/hsh/homesweethome/FurnitureCategoryFragment.java
@@ -1,15 +1,18 @@
 package com.example.hsh.homesweethome;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.hsh.homesweethome.Models.CategoryFurniture;
@@ -18,6 +21,7 @@ import com.example.hsh.homesweethome.Models.Furniture;
 import java.util.ArrayList;
 
 import static com.google.sceneform_animation.bi.as;
+import static com.google.sceneform_animation.bi.f;
 
 public class FurnitureCategoryFragment extends Fragment {
 
@@ -27,6 +31,8 @@ public class FurnitureCategoryFragment extends Fragment {
     private RecyclerViewAdapterCategoryFurnitures recyclerViewAdapterCategoryFurnitures;
     private RecyclerViewAdapterHorizontalLocations recyclerViewAdapterHorizontalLocations;
     private ArrayList<String> locations;
+    private ImageView furnitureFilter;
+    private CardView apply_button;
 
     public FurnitureCategoryFragment() {
     }
@@ -47,7 +53,7 @@ public class FurnitureCategoryFragment extends Fragment {
         categoryFurniture = (CategoryFurniture) fragmentArgs.getSerializable("category_furnitures");
         locations = fragmentArgs.getStringArrayList("locations");
 
-
+        furnitureFilter = view.findViewById(R.id.filter_furniture);
         furnitureCategory = view.findViewById(R.id.furnitureCategory);
         locationsHorizontalRecyclerView = view.findViewById(R.id.locations_horizontal_recycler);
         categoryFurnitureRecyclerView = view.findViewById(R.id.category_furnitures_recycler_view);
@@ -64,6 +70,22 @@ public class FurnitureCategoryFragment extends Fragment {
 
         furnitureCategory.setText(categoryFurniture.getFurnitureCategory());
 
+        furnitureFilter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Dialog filterDialog = new Dialog(getContext());
+                filterDialog.setContentView(R.layout.filter_dialog);
+                apply_button = filterDialog.findViewById(R.id.apply_button);
+                apply_button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        filterDialog.dismiss();
+                    }
+                });
+                filterDialog.show();
+
+            }
+        });
 
     }
 }
